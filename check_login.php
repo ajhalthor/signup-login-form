aee7f0f19adf3f7a473bdf16735d5e4449b414eb
Ajay Halthor <Ajay@Ajays-MacBook-Pro.local>
2016-05-30 01:39:51 +0530

    It just checks if the user has already logged in

--- check_login.php
+++ check_login.php
@@ -1,9 +1,8 @@
 <?php
 	include_once 'session.inc.php';
 	if(!isset($_SESSION['student_login'])){
-		//////echo json_encode(array("success" => true, "message" => 'Valid Details entered', "url" => 'student-portal.php#/'));
-		//////return;
-		//header("location:student-portal.php#/");
+		echo json_encode(array("success" => true, "message" => 'Valid Details entered', "url" => 'student-portal.php#/'));
+		return;
 	}
 
 ?>
